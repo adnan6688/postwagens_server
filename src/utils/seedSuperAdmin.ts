3fa19090717f968b1fd33697e79097d955f22885
adnan6688 <adnanfarukgolam680@gmail.com>
2026-07-15 09:25:08 +0600

    updated

--- src/utils/seedSuperAdmin.ts
+++ src/utils/seedSuperAdmin.ts
@@ -1,19 +1,21 @@
 import env from "../config/env"
 import { Role } from "../modules/users/user.interface"
 import User from "../modules/users/user.model"
-
+import bcrypt from 'bcrypt'
 
 export const seedSuperAdmin = async () => {
 
 
     const ckAdmin = await User.findOne({ email: env.EMAIL_USER })
+    const hash = await bcrypt.hash(env.EMAIL_PASSWORD as string, Number(env.BCRYPT_SALT_ROUND))
     if (!ckAdmin) {
-    
+
         await User.create({
             email: env.EMAIL_USER,
-            password: env.EMAIL_PASSWORD, // plain password
-            fullName: 'Golam Faruk Adnna',
+            password: hash, // plain password
+            fullName: 'Joseph Murray',
             role: Role.ADMIN,
+            isVerified : true,
             auths: [
                 {
                     provider: 'AuthProvider',
